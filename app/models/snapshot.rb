6ed88f56bc67a87093feb564bdfbddcbdfd0a72c
Michael <1938084+Michi0301@users.noreply.github.com>
2018-06-13 21:55:19 +0200

    Adds total_rating calculation.

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -1,3 +1,12 @@
 class Snapshot < ApplicationRecord
   belongs_to :company
+
+  def total_rating
+    ((employee_rating_count * employee_rating_avg +
+      applicant_rating_count * applicant_rating_avg +
+      trainee_rating_count * trainee_rating_avg)/
+      (employee_rating_count + 
+      applicant_rating_count + 
+      trainee_rating_count)).round(2)
+  end
 end
