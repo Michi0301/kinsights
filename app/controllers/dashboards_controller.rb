8ae2385a2b73c3b6f1eaa5c1e3754df0a892a494
Michael <1938084+Michi0301@users.noreply.github.com>
2018-06-21 21:29:35 +0200

    Removes not used parameters.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,5 @@
 class DashboardsController < ApplicationController
   def show
     @companies = Company.order(:name)
-    @start_date = Review.order(:publish_date).first.publish_date
-    @end_date = Review.order(:publish_date).last.publish_date
   end
 end
--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,11 +1,11 @@
 module DashboardsHelper
-  def render_daily_avg_review_rating(company, start_date, end_date)
+  def render_daily_avg_review_rating(company)
     line_chart [
       { name: 'total rating', data: DataSet.chart_data_for(company, 'daily_avg_review_rating', 'daily_avg_review_rating') }
     ], min: 0, max: 5
   end
 
-  def render_daily_avg_review_count(company, start_date, end_date)
+  def render_daily_avg_review_count(company)
     line_chart [
       { name: 'total rating count', data: DataSet.chart_data_for(company, 'daily_avg_review_count', 'daily_avg_review_count') }
     ]
